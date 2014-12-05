833fd79e672224021610cf8048db4850049983f0
ohkayrg <ohkayrg@gmail.com>
2014-12-04 19:54:47 -0500

    modified GetLatiLongi function so then it can take in any txt file

--- code/GetLatiLongi.py
+++ code/GetLatiLongi.py
@@ -10,5 +10,15 @@ def GetLatiLongi():
         results[nline[0]] = map(int, nline[1].split(','))
     return results
 
+def GetLatiLongi1(name):
+	import os 
+	path=os.path.dirname(os.path.abspath(__file__))
+	f=open(path+ '/'+name, 'r')
+	results=dict()
+	for line in f:
+		nline=line.strip().split('\t')
+		results[nline[0]]=map(int, nline[1].split(','))
+	return results 
+
 if __name__ == "__main__":
     print GetLatiLongi()
