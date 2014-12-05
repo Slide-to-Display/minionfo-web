34f90a154d0d65e1c1160e1d1a71764337ec5967
ohkayrg <ohkayrg@gmail.com>
2014-12-04 19:53:27 -0500

    modified GetStateList() so then it can take in any txt file

--- code/GetStateList.py
+++ code/GetStateList.py
@@ -10,6 +10,16 @@ def GetStateList():
         result.append((nline[0].lower(), nline[0]))
     return result
 
+def GetStateList1(name):
+    import os
+    path = os.path.dirname(os.path.abspath(__file__))
+    f = open(path + "/"+name, 'r')
+    result = list()
+    for line in f:
+        nline = line.split('\t')
+        result.append((nline[0].lower(), nline[0]))
+    return result	
+
 
 if __name__ == "__main__":
     print GetStateList()
